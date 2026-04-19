893399c6249af7c2ace99daabc677fb3fe8c1e9f
magic <1362748167@qq.com>
2026-04-19 12:29:35 +0800

    ESCVehicle数据集训练最终

--- train.py
+++ train.py
@@ -1,5 +1,14 @@
 #训练
 from ultralytics import YOLO
 import ultralytics.nn.tasks
-model = YOLO('/home/mjy/ultralytics/yaml/PC2f_MPF_yolov8n.yaml')
-results = model.train(data='/home/mjy/ultralytics/data/drone2.yaml',batch=16,epochs=200)
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/PC2f_MPF_yolov8s.yaml')
+model.load("/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt")
+results = model.train(
+    data='/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/data/escvehicle.yaml',
+    batch=64,
+    epochs=200,
+    imgsz=704,
+    device='1,2,3,4',
+    project='/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/escvehicle_runs',
+    task="obb"
+)
