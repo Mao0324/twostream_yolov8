8bc4d95ec90e89793d1ef7fbefb282aabbf7545d
Mao0324 <2116030319@s.upc.edu.cn>
2026-05-07 20:08:54 +0800

    update: assafusion config, training script and tools

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assa_lite.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_sparsefusion.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
 model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
@@ -11,10 +11,10 @@ model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-traine
 # 3) 训练
 results = model.train(
     data='/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/data/dronevehicle.yaml',
-    batch=64,
+    batch=128,
     epochs=100,
     imgsz=640,
-    device='0,1,2,3,4,5,6,7',
-    project='dronevehicle_runs_assaaddfusion',
+    device='6,7',
+    project='dronevehicle_runs_assafusion',
     task='obb'
 )
