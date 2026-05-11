9b525f793926a9888dd61a759ddb006ab8fde641
Mao0324 <2116030319@s.upc.edu.cn>
2026-05-11 15:01:42 +0800

    entmax train and test results ❌

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_ablation_full.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_ablation_entmax.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
 model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
@@ -15,6 +15,7 @@ results = model.train(
     epochs=100,
     imgsz=640,
     device='3,4,5,6',
-    project='dronevehicle_runs_assafusion',
+    project='dronevehicle_runs_ablation_entmax_gate',
+    name='entmax',
     task='obb'
 )
