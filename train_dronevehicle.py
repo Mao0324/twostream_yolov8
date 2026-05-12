9a491dd477c9a69646b493f99c53c9e2a715651f
Mao0324 <2116030319@s.upc.edu.cn>
2026-05-12 11:55:47 +0800

    dronevehicle_runs_ablation_gate train and test results

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_ablation_entmax.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_ablation_gate.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
 model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
@@ -14,8 +14,8 @@ results = model.train(
     batch=128,
     epochs=100,
     imgsz=640,
-    device='3,4,5,6',
+    device='2,3,5,6',
     project='dronevehicle_runs_ablation_entmax_gate',
-    name='entmax',
+    name='gate',
     task='obb'
 )
