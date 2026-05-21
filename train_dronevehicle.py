3f092667f419ddad28aa66933f8fa7e0e705e644
Mao0324 <2116030319@s.upc.edu.cn>
2026-05-21 09:52:36 +0800

    assafusion_c2f_fusion_conv_init_scale1e-2 train and test results

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -15,6 +15,6 @@ results = model.train(
     epochs=100,
     imgsz=640,
     device='5,6',
-    project='dronevehicle_runs_assafusion_c2f_fusion_conv',
+    project='dronevehicle_runs_assafusion_c2f_fusion_conv_init_scale1e-2',
     task='obb'
 )
