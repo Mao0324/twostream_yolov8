2fb6370083aed5e7b6a714419281543c36b56c6a
Mao0324 <2116030319@s.upc.edu.cn>
2026-06-04 17:09:33 +0800

    IRGuidedARASSARIFusionIR 作为主干锚点，RGB 从 IR 获得局部增强和跨模态注意力补充训练测试结果上传

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_cgarassa_p4p5_postc2f.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_irguided_arassa_p4_postc2f.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
 model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
@@ -15,6 +15,6 @@ results = model.train(
     epochs=100,
     imgsz=640,
     device='4,5',
-    project='dronevehicle_runs_assafusion_c2f_fusion_conv_p4p5cgarassa',
+    project='dronevehicle_runs_assafusion_c2f_fusion_conv_P4irguidedarassa',
     task='obb'
 )
