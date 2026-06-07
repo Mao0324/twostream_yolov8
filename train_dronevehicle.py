afbde92fa85a96fe2d530952d7fa059a2f37cea2
Mao0324 <2116030319@s.upc.edu.cn>
2026-06-07 16:37:38 +0800

    P3Preserve+P3 检测输入增强Refine 训练、测试结果上传

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_irguided_arassa_p3_postc2f.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_p3_refine_preserve_postc2f.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
-model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
+model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream_p3_refine_preserve.pt')
 
 # 3) 训练
 results = model.train(
@@ -15,6 +15,6 @@ results = model.train(
     epochs=100,
     imgsz=640,
     device='4,5',
-    project='dronevehicle_runs_assafusion_c2f_fusion_conv_P3irguidedarassa',
+    project='dronevehicle_runs_assafusion_c2f_fusion_conv_p3_refine_preserve',
     task='obb'
 )
