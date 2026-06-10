5f4d340af36aacf0706b132735da63f57d7b34aa
Mao0324 <2116030319@s.upc.edu.cn>
2026-06-10 09:17:55 +0800

    dualbranch p4only posscale localres train and test results

--- train_dronevehicle.py
+++ train_dronevehicle.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import ultralytics.nn.tasks  # noqa: F401
 
 # 1) 模型结构
-model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_dualbranch_postc2f.yaml')
+model = YOLO('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/yaml/yolov8_twostream_obb_assafusion_dualbranch_p4only_postc2f.yaml')
 
 # 2) 预训练权重（如不存在可注释掉）
 model.load('/home/biiteam/Storage-4T/biiteam/MCONG/TwoStream_Yolov8_2/pre-trained/yolov8s-obb_twostream.pt')
@@ -14,7 +14,7 @@ results = model.train(
     batch=64,
     epochs=100,
     imgsz=640,
-    device='2,3',
-    project='dronevehicle_runs_assafusion_c2f_fusion_conv_dual_sparse_dense_onlyp3p4_p5assa',
+    device='1,2',
+    project=' dronevehicle_runs_assafusion_c2f_fusion_conv_dualbranch_p4only_posscale_localres',
     task='obb'
 )
